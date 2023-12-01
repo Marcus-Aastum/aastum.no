5ecf0e3f62e9a1f6b043cdbf61cac10d12827fde
Marcus <marcusaastum@gmail.com>
2023-12-01 22:50:06 +0100

    Initial commit
    Initial commit on new website design

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-function klikk(knapp){
-    knapp.style.backgroundColor = "green";
-    window.location.href = "old/index.html";
+fileName = document.getElementById("navbar").getAttribute("w3-include-html");
 
-}
+//Make XMLHttpRequest with the attribute value
+xmlHttp = new XMLHttpRequest();
+xmlHttp.onreadystatechange = function() {
 
-function sm00th(knapp) {
-    knapp.style.backgroundColor = "green";
-    window.location.href = "sm00th/index.html";
-    console.log("smOOth");
+   //If the request is successful, load the new HTML. Else throw 404 error and try again
+   if (this.readyState == 4) {
+      if (this.status == 200) {document.getElementById("navbar").innerHTML = this.responseText;}
+      if (this.status == 404) {document.getElementById("navbar").innerHTML = "Error 404";}
+
+   /* Remove the attribute and invoke the function again*/
+   document.getElementById("navbar").removeAttribute("w3-include-html");
+   addHTML();
+   }
 }
+xmlHttp.open("GET", fileName, true);
+xmlHttp.send();
